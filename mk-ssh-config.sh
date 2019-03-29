f6943318a6fc5385cc907cc3de59e8f92f06abdf
Sebastian Wagner <sebastian.wagner@suse.com>
2019-03-29 10:31:42 +0100

    use passwordless ssh
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- mk-ssh-config.sh
+++ mk-ssh-config.sh
@@ -1,7 +1,7 @@
 cat <<EOF > ssh_config
 Host $(echo $(terraform output -json | jq -r '.ips.value[][]'))
    User root
-   #IdentityFile $PWD/id_shared
+   IdentityFile $PWD/ssh_id_shared
    StrictHostKeyChecking no
    UserKnownHostsFile=/dev/null
 EOF
