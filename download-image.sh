kubic-project/kubic-terraform-kvm

commit 59b52671d4a62efeaa1306be73411fe54573b21f

Fix terraform, add download-image.sh
Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

download-image.sh
@@ -1,0 +1,7 @@
+#!/bin/bash
+
+base_url="https://download.opensuse.org/repositories/devel:/kubic:/images/openSUSE_Tumbleweed/"
+name=$(curl --silent "$base_url" | pandoc -f html -t plain | egrep -e 'kubeadm-cri-o-kvm-and-xen.*qcow2.xz$')
+
+wget -O kubic.qcow2.xz "$base_url$name"
+xz -f -v --decompress kubic.qcow2.xz

.gitignore
@@ -1,6 +1,6 @@
 .swp
 .terraform/
-kubic-kvm
+kubic.qcow2
 terraform.log
 terraform.tfstate
 terraform.tfstate.backup

commoninit.cfg
@@ -1,7 +1,7 @@
 #cloud-config
 
 # set locale
-locale: en_GB.UTF-8
+locale: en_US.UTF-8
 # set timezone
 timezone: Etc/UTC
 
@@ -17,7 +17,7 @@ disable_root: false
 runcmd:
   - systemctl enable kubelet.service
   - systemctl enable --now crio.service
-  - "sed -i 's!KUBELET_EXTRA_ARGS=!KUBELET_EXTRA_ARGS=--cni-bin-dir=/usr/lib/cni!g' /etc/sysconfig/kubelet"
+  - sed -i 's!KUBELET_EXTRA_ARGS="!KUBELET_EXTRA_ARGS="--cni-bin-dir=/usr/lib/cni !g' /etc/sysconfig/kubelet
   - sed  -i '/PermitRootLogin/s/.*/PermitRootLogin yes/' /etc/ssh/sshd_config
   - systemctl restart sshd
   - modprobe ip_vs_rr ip_vs_sh ip_vs_wrr nf_conntrack_ipv4

kubic-kvm.tf
@@ -9,7 +9,7 @@ variable "count_vms" {
 
 resource "libvirt_volume" "kubic_image" {
   name   = "kubic_image"
-  source = "https://download.opensuse.org/repositories/devel:/kubic:/images:/experimental/images_devel_kubic/openSUSE-Tumbleweed-Kubic.x86_64-15.0-kubeadm-cri-o-OpenStack-Cloud-Build11.13.qcow2"
+  source = "./kubic.qcow2"
 }
 
 resource "libvirt_volume" "os_volume" {

commit 5baaeb123ebca73b89c3d09cde101fea7fe58086

download-image: Handle both qcow2 and qcow2.xz files
Before this change, the script assumed that kubeadm-cri* image is
always distributed in form of .xz archive, which is not always true.
Sometimes only unarchived .qcow2 image is available.

Fixes: 59b52671d4a6 ("Fix terraform, add download-image.sh")

Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

download-image.sh
@@ -3,7 +3,15 @@
 set -e
 
 base_url="https://download.opensuse.org/repositories/devel:/kubic:/images/openSUSE_Tumbleweed/"
-name=$(curl --silent "$base_url" | pandoc -f html -t plain | egrep -e 'kubeadm-cri-o-kvm-and-xen.*qcow2.xz$')
+name_xz=$(curl --silent "$base_url" | pandoc -f html -t plain | egrep -e 'kubeadm-cri-o-kvm-and-xen.*qcow2.xz$' || true)
+name=$(curl --silent "$base_url" | pandoc -f html -t plain | egrep -e 'kubeadm-cri-o-kvm-and-xen.*qcow2$' || true)
 
-wget -O kubic.qcow2.xz "$base_url$name"
-xz -f -v --decompress kubic.qcow2.xz
+if [[ ! -z "$name_xz" ]]; then
+    wget -O kubic.qcow2.xz "$base_url$name_xz"
+    xz -f -v --decompress kubic.qcow2.xz
+elif [[ ! -z "$name" ]]; then
+    wget -O kubic.qcow2 "$base_url$name"
+else
+    echo "Error: Could not find kubeadm-cri-o-kvm-and-xen image on $base_url"
+    exit 1
+fi