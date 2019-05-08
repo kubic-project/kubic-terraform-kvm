b9d9cfd93e797181f7675d9f294fb1f652e51f97
Sebastian Wagner <sebastian.wagner@suse.com>
2019-05-08 14:25:19 +0200

    removed .xz code.
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- download-image.py
+++ download-image.py
@@ -5,16 +5,11 @@ from functools import reduce
 from urllib.request import urlretrieve, urlopen
 
 base_url="https://download.opensuse.org/repositories/devel:/kubic:/images/openSUSE_Tumbleweed/"
+image_name='openSUSE-MicroOS.x86_64-Kubic-kubeadm-kvm-and-xen.qcow2'
 
 def download_file(url) -> str:
     with urlopen(url) as f:
         return f.read().decode('utf-8')
-
-def image_name():
-    html = download_file(base_url)
-    name = 'openSUSE-MicroOS.x86_64-Kubic-kubeadm-kvm-and-xen.qcow2'
-    name_xz = 'openSUSE-MicroOS.x86_64-Kubic-kubeadm-kvm-and-xen.qcow2.xz'
-    return name_xz if name_xz in html else name
 
 def local_sha256(filename) -> str:
     sha256 = hashlib.sha256()
@@ -26,7 +21,7 @@ def remote_sha256(url):
     data = download_file(url + '.sha256')
     return [l for l in data.splitlines() if '.qcow2' in l][0].split()[0]
 
-def donload_image(url: str, dst):
+def download_image(url: str, dst):
     last_percent_reported = 0
 
     def reporthook(count, blockSize, totalSize):
@@ -45,12 +40,9 @@ def donload_image(url: str, dst):
 
 
 if __name__ == '__main__':
-    name = image_name()
-    dst = 'kubic.qcow2.xz' if name.endswith('xz') else 'kubic.qcow2'
-    url = base_url + name
+    dst = 'kubic.qcow2'
+    url = base_url + image_name
     if local_sha256(dst) == remote_sha256(url):
         print('already downloaded.')
     else:
-        donload_image(url, dst)
-    if name.endswith('.xz'):
-        os.system('xz -k -f -v --decompress kubic.qcow2.xz')
+        download_image(url, dst)
