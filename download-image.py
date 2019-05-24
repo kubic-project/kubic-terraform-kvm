kubic-project/kubic-terraform-kvm

commit f46bfaa5c87cb1b792282e02c3657af751a3541d

download-image.sh: Load only new images
Fixes #4

Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

download-image.py
@@ -1,0 +1,56 @@
+#!/usr/bin/python3
+import hashlib
+import os
+from functools import reduce
+from urllib.request import urlretrieve, urlopen
+
+base_url="https://download.opensuse.org/repositories/devel:/kubic:/images/openSUSE_Tumbleweed/"
+
+def download_file(url) -> str:
+    with urlopen(url) as f:
+        return f.read().decode('utf-8')
+
+def image_name():
+    html = download_file(base_url)
+    name = 'openSUSE-MicroOS.x86_64-Kubic-kubeadm-kvm-and-xen.qcow2'
+    name_xz = 'openSUSE-MicroOS.x86_64-Kubic-kubeadm-kvm-and-xen.qcow2.xz'
+    return name_xz if name_xz in html else name
+
+def local_sha256(filename) -> str:
+    sha256 = hashlib.sha256()
+    with open(filename, 'rb') as f:
+        reduce(lambda _, c: sha256.update(c), iter(lambda: f.read(sha256.block_size * 128), b''), None)
+    return sha256.hexdigest()
+
+def remote_sha256(url):
+    data = download_file(url + '.sha256')
+    return [l for l in data.splitlines() if '.qcow2' in l][0].split()[0]
+
+def donload_image(url: str, dst):
+    last_percent_reported = 0
+
+    def reporthook(count, blockSize, totalSize):
+        nonlocal last_percent_reported
+        percent = int(count * blockSize * 100 / totalSize)
+
+        if last_percent_reported != percent:
+            if percent % 5 == 0:
+                print("%s%%" % percent)
+
+            last_percent_reported = percent
+
+    print("downloading " + url)
+
+    urlretrieve(url, dst, reporthook=reporthook)
+
+
+if __name__ == '__main__':
+    name = image_name()
+    dst = 'kubic.qcow2.xz' if name.endswith('xz') else 'kubic.qcow2'
+    url = base_url + name
+    if local_sha256(dst) == remote_sha256(url):
+        print('already downloaded.')
+    else:
+        donload_image(url, dst)
+    if name.endswith('.xz'):
+        os.system('xz -k -f -v --decompress kubic.qcow2.xz')

download-image.sh
@@ -1,17 +1,0 @@
-#!/bin/bash
-
-set -e
-
-base_url="https://download.opensuse.org/repositories/devel:/kubic:/images/openSUSE_Tumbleweed/"
-name_xz=$(curl --silent "$base_url" | pandoc -f html -t plain | egrep -e 'kubeadm-cri-o-kvm-and-xen.*qcow2.xz$' || true)
-name=$(curl --silent "$base_url" | pandoc -f html -t plain | egrep -e 'kubeadm-cri-o-kvm-and-xen.*qcow2$' || true)
-
-if [[ ! -z "$name_xz" ]]; then
-    wget -O kubic.qcow2.xz "$base_url$name_xz"
-    xz -f -v --decompress kubic.qcow2.xz
-elif [[ ! -z "$name" ]]; then
-    wget -O kubic.qcow2 "$base_url$name"
-else
-    echo "Error: Could not find kubeadm-cri-o-kvm-and-xen image on $base_url"
-    exit 1
-fi

commit e0e41866b64debd658e315cd3f952788a069a06a

download-image: Perform sha256 sum check when file exists
Before this change, download-image.py script was trying to open
kubic.qcow2 file for sha256 sum check regardless of whether the file
existed.

Fixes: #9
Fixes: f46bfaa5c87c ("download-image.sh: Load only new images")

Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

download-image.py
@@ -42,7 +42,7 @@ def download_image(url: str, dst):
 if __name__ == '__main__':
     dst = 'kubic.qcow2'
     url = base_url + image_name
-    if local_sha256(dst) == remote_sha256(url):
+    if os.path.isfile(dst) and local_sha256(dst) == remote_sha256(url):
         print('already downloaded.')
     else:
         download_image(url, dst)